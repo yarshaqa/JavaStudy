b241e74fece1965ffeacd7acb3960c96a8447930
Yaroslav Shabatura <yarshaqa@gmail.com>
2020-04-08 19:08:59 +0300

    fixed

--- src/main/java/codingbat/diff21.java
+++ src/main/java/codingbat/diff21.java
@@ -1,17 +1,16 @@
 package codingbat;
 
+
 public class diff21 {
 
 
     public int diff21(int n) {
 
 
-
-        if(n <=21){
+        if (n <= 21) {
             return (Math.abs(21 - n));
-        }
-        else{
-            return(2*(Math.abs(21-n)));
+        } else {
+            return (2 * (Math.abs(21 - n)));
         }
     }
 
